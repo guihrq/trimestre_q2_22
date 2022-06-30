b3a87595f2d0c34aa1ba39eefae69fb26dc642ce
Guilherme Henrique Borges Silva <gui.hrq@gmail.com>
2022-06-30 18:56:36 -0300

    Second commit

--- hidden/wp-content/themes/hidden/single-display.php
+++ hidden/wp-content/themes/hidden/single-display.php
@@ -5,19 +5,6 @@ get_header();
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		<section class="container produto_item animar-interno">
-			<div class="grid-11">
-				<img src="<?php the_field('foto_produto_1'); ?>" alt="Hidden <?php the_title(); ?>">
-				<h2><?php the_title(); ?></h2>
-			</div>
-			<div class="grid-5 produto_icone"><img src="<?php the_field('icone_produto'); ?>" alt="Icone <?php the_title(); ?>"></div>
-			<div class="grid-8"><img src="<?php the_field('foto_produto_2'); ?>" alt="Hidden <?php the_title(); ?>"></div>
-			<div class="grid-8 produto_info">
-				<?php the_content(); ?>
-			</div>
-		</section>
-
-		<?php include(TEMPLATEPATH . "/includes/produtos-orcamento.php"); ?>
 		
 <?php endwhile; else: endif; ?>
 
